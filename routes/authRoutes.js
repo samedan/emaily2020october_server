59cd9f10d4be8dc9548c21b6afc2a4896b535e36
Daniel Popescu <same.dan@gmail.com>
2020-10-20 16:06:03 +0200

    Finished auth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,19 +1,33 @@
-const passport = require('passport');
+const passport = require("passport");
 
 module.exports = (app) => {
-    app.get('/auth/google', passport.authenticate('google', {
-        scope: ['profile', 'email']
-    }))
-    
-    app.get('/auth/google/callback', passport.authenticate('google'));
+  app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+      scope: ["profile", "email"],
+    })
+  );
 
-    app.get('/api/logout', (req, res) => {
-        req.logout();
-        res.send(req.user);
-    });
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google"),
+    // what to do after auth
+    (req, res) => {
+      res.redirect("/surveys");
+    }
+  );
 
-    app.get('/api/current_user', (req, res) => {
-        res.send(req.user);
-    });
-}
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    // what to do after auth
 
+    res.redirect("/");
+  });
+
+  app.get(
+    "/api/current_user",
+    (req, res) => {
+      res.send(req.user);
+    }
+  );
+};
