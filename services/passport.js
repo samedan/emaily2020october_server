19b70b56818ac575ed834f161744ee9009ddef26
Daniel Popescu <same.dan@gmail.com>
2020-10-20 16:24:35 +0200

    Auth Not fixed yet

--- services/passport.js
+++ services/passport.js
@@ -1,9 +1,10 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const mongoose = require('mongoose');
-const keys = require('../config/keys');
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20")
+  .Strategy;
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
 
-const User = mongoose.model('users');
+const User = mongoose.model("users");
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
@@ -19,22 +20,31 @@ passport.use(
   new GoogleStrategy(
     {
       clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback',
+      clientSecret:
+        keys.googleClientSecret,
+      callbackURL:
+        "/auth/google/callback",
       proxy: true,
     },
-    async (accessToken, refreshToken, profile, done) => {
-        const existingUser = await User.findOne({ googleId: profile.id })
-        if (existingUser) {
-          // we already have a record with the given profile ID
-          done(null, existingUser);
-        } else {
-          // we don't have a user record with this ID, make a new record!
-          const user =  await new User({ googleId: profile.id })
-            .save();
-            done(null, user);
-        }
-      
+    async (
+      accessToken,
+      refreshToken,
+      profile,
+      done
+    ) => {
+      const existingUser = await User.findOne(
+        { googleId: profile.id }
+      );
+      if (existingUser) {
+        // we already have a record with the given profile ID
+        done(null, existingUser);
+      } else {
+        // we don't have a user record with this ID, make a new record!
+        const user = await new User({
+          googleId: profile.id,
+        }).save();
+        done(null, user);
+      }
     }
   )
 );
